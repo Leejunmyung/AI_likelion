1b3bd1655f018cb60f530e3d116b3c77a1aa484a
junmyung <wnsaud9322@naver.com>
2021-06-14 16:22:29 +0900

    tk commit

--- week1/05_11day/01_11-1.py
+++ week1/05_11day/01_11-1.py
@@ -14,11 +14,25 @@ def click():
     textbox.delete(0.0, END)
 
     try:
-        def_word = dat.loc[dat['area'] == word, 'def'].values[0]
+        def_word = dat.loc[dat['area'] == word, 'acd'].values[0]
+
     except:
-        def_word = "단어의 뜻을 찾을 수 없음"
+        def_word = "결과를 찾을 수 없음"
 
     textbox.insert(END, def_word)
+
+
+def click2():
+    word = entry.get()
+
+    dtbox.delete(0.0, END)
+
+    try:
+        wow = dat.loc[dat['area'] == word, 'death'].values[0]
+    except:
+        wow = "결과를 찾을 수 없음"
+
+    dtbox.insert(END, wow)
 
 window = Tk()
 window.title("My Dictionary")
@@ -31,17 +45,27 @@ label.grid(row=0, column=0, sticky=W)
 entry = Entry(window,width=25, bg="light yellow")
 entry.grid(row=1, column=0, sticky=W)
 
-# 03 제출 버튼
-btn = Button(window, width=15, text="제출", bg="light green", command=click)
+# 03 결과 버튼
+btn = Button(window, width=7, text="발생건수", bg="light green", command=click)
 btn.grid(row=1, column=1,columnspan=2 ,sticky=W)
 
-# 04 설명 레이블 - 의미
-mean = Label(window, text="\n단어의 의미")
+btn2 = Button(window, width=7, text="사망수", bg="light blue", command=click2)
+btn2.grid(row=1, column=2,columnspan=2 ,sticky=W)
+
+# 04 결과 레이블 - 발생건수
+mean = Label(window, text="\n발생건수")
 mean.grid(row=3, column=0, sticky=W)
 
+# 05 결과 레이블 - 사망자수
+result = Label(window, text="\n사망자수")
+result.grid(row=3, column=1, sticky=W)
+
 # 05 텍스트 박스 입력 상자
-textbox = Text(window, width=50,height=7,wrap=WORD, bg="light yellow")
+textbox = Text(window, width=10,height=2,wrap=WORD, bg="light yellow")
 textbox.grid(row=4, column=0, sticky=W)
+
+dtbox = Text(window, width=10,height=2,wrap=WORD, bg="light yellow")
+dtbox.grid(row=4, column=1, sticky=W)
 
 # 메인 반복문 실행
 window.mainloop()
